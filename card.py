458c8ac55163bb7dc9a3b6778faf3253478f6857
Hannah Lu <hannahlu1207@yahoo.com>
2022-01-30 12:08:48 -0800

    update classes, create testing deck
    Co-authored-by: Becca Verghese <bverghese@g.hmc.edu>

--- card.py
+++ card.py
@@ -1,80 +1,53 @@
 from datetime import datetime, timedelta
 from supermemo2 import SMTwo
-import pandas as pd
-
-from webscrape import get_media
+from pandas import DataFrame as df
 
 
 class HistoricalPerformance():
     def __init__(self):
-        self.history = pd.DataFrame(columns=["date", "quality"])
+        self.history = df(columns=["date", "quality"])
 
     def add_record(self, date, quality):
-        self.history.append(
-            {"date": date, "quality": quality}, 
-            ignore_index=True)
+        self.history.append({"date": date, "quality": quality})
 
 class Card():
 
     def __init__(self, term, importance=1, tags = []):
-        # TODO: create subclass? to account for different directions of review
-        # (i.e. ASLtoEng or EngtoASL)
         self.english = term
-        self.media = self.getMedia() # list of links to mp4s, images, etc
+        self.media = [] # list of links to mp4s, images, etc
         self.description = ""
-        self.hint = "" #Need to find hint in ASL Browser notes text file
+        self.hint = ""
         # TODO: try to get hint/description from the text file
         self.importance = importance
         self.history = HistoricalPerformance()
-        self.sm2Data = None # obj of type SMTwo
+        self.sm2Data = None # type SMTwo
         # self.SM2data will contain the easiness score, interval, and next 
         # review date
         # update self.sm2Data once the user is tested on this word to be 
         # SMTwo.first_review(quality, datetime.today())
         self.lastReviewDate = None
         # This has the format "datetime.datetime(2022, 1, 13, 16, 50, 31, 568809)"
-        self.nextReviewDate = datetime.today()
+        self.nextReviewDate = None
         self.tags = tags
-        # self.qualEngtoASL = 0
-        # self.qualASLtoEng = 0
-        self.quality = 0
     
-    def getMedia(self):
-        media = []
-        # placeholder
-        return media
-
     def getQuality(self):
-        """ placeholder function for now. get the quality that the user selects
-            when the term is tested 
-        """
+        """ placeholder function for now """
         quality = None 
-        self.quality = quality
-        # if form == "EngtoASL": self.qualEngtoASL = quality
-        # elif form == "ASLtoEng": self.qualASLtoEng = quality
         return quality
 
     def reviewCard(self):
-        self.getQuality()
+        quality = self.getQuality()
         self.lastReviewDate = datetime.today()
         if self.sm2Data == None:
-            self.sm2Data = SMTwo.first_review(self.lastReviewDate, self.quality)
+            self.sm2Data = SMTwo.first_review(self.lastReviewDate, quality)
         else: 
-            self.sm2Data.review(self.lastReviewDate, self.quality)
+            self.sm2Data.review(self.lastReviewDate, quality)
         interval = timedelta(days = self.sm2Data.interval) 
-        # TODO: update algorithm for shorter intervals (e.g. 10 min)
+        # TODO: update algorithm to all for shorter intervals (e.g. 10 min)
         self.nextReviewDate = self.lastReviewDate + interval
     
     # create function to get Hint
-    def getHint(self):
-        """placeholder, when user clicks 'Get Hint' button, hint appears
-            we update the hint variable from the ASL Browser notes text file
-        """
-        #Create dictionary from the ASL Browser notes which stores the mp4 file and hint text
-        return self.hint
-
-
-    
+    # create function to display card
 
     
     
