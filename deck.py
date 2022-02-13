c12816a29b9c4d5af04a64ea37c8951fd5f804f2
Hannah Lu <hannahlu1207@yahoo.com>
2022-02-12 20:57:39 -0800

    fix merge issues

--- deck.py
+++ deck.py
@@ -2,45 +2,42 @@ from card import *
 from datetime import datetime, timedelta
 import pandas as pd
 
+
 class Deck():
-
     def __init__(self):
-<<<<<<< HEAD
-        self.cards = pd.DataFrame(columns=["next review date", "term", "quality", "card"]) 
+        self.cards = pd.DataFrame(
+            columns=["next review date", "term", "quality", "card"])
         # self.allCards = pd.DataFrame(columns= ['Card'])
-=======
-        self.cards = pd.DataFrame(columns=["next review date", "term", "quality"]) 
->>>>>>> 561e5b628ba1260dff37e03c1e5bbaa16d57c440
-        self.learn_today = [] # list of card objects
+        self.learn_today = []  # list of card objects
         self.size = 0
-        # TODO add this 
-        self.progress = None # % of cards that have 4 or higher quality score / total cards 
+        # TODO add this
+        self.progress = None  # % of cards that have 4 or higher quality score / total cards
 
-        
     def get_todays_cards(self):
-        todays_cards = self.cards[self.cards['next review date'] == datetime.today()]['term'].tolist()
+        todays_cards = self.cards[self.cards['next review date'] ==
+                                  datetime.today()]['term'].tolist()
         return todays_cards
-    
-    def addCard(self, term, importance=1, tags = []):
+
+    def addCard(self, term, importance=1, tags=[]):
         card = Card(term, importance, tags)
         self.cards = self.cards.append(
             {
-                "next review date" : card.nextReviewDate, 
-                "term" : term, 
+                "next review date": card.nextReviewDate,
+                "term": term,
                 "quality": card.quality,
-                "card": card # temporary until we set up a server/database
-            }, 
+                "card": card  # temporary until we set up a server/database
+            },
             ignore_index=True)
         #
         #self.allCards = self.allCards.append({'Card': card}, ignore_index = True)
         self.learn_today.append(card)
         self.size += 1
-    
+
     def deleteCard(self, card):
         self.cards.drop([card.nextReviewDate, card.english, card.quality])
         self.size -= 1
-    
-    # TODO add this 
+
+    # TODO add this
     def updateProgress(self):
         return 0
 
